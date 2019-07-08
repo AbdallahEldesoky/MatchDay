ecf92a969db2415f3ce6ae026f485cda2a2f8feb
Abdallah Eldesoky <eldesoky94@gmail.com>
2019-07-09 00:38:17 +0200

    add LBTAComponent and TRON

---
+++ MatchDay/CustomTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  CustomTabBarController.swift
+//  MatchDay
+//
+//  Created by Abdallah on 7/8/19.
+//  Copyright © 2019 Abdallah Eldesoky. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
